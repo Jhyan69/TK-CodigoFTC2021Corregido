170bcddfe4301361335fb80ba59610d6581e950e
Jan <jangorozpesacristan@gmail.com>
2022-11-18 10:23:12 -0600

    Potencia del shooter
    Cambié la potencia a -0.75 y comente los parámetros ideales para esta. Solo prueben el shooter cuando un líder esté presente

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -10,10 +10,12 @@ public class Turret {
         if (Robot.control.readJoystickButtons(Constantes.LG_B2)){
         
             turretTalon.set(ControlMode.PercentOutput, -1);  
+            turretTalon.set(ControlMode.PercentOutput, -0.75);  
         }
 
         else if(Robot.control.readPS4Buttons(2)){ //Botón A 
             turretTalon.set(ControlMode.PercentOutput, -0.70);
+            turretTalon.set(ControlMode.PercentOutput, -0.75); //mantengan la potencia del shooter abajo de -0.90 y arriba de -0.50
         }
         else{
             turretTalon.set(ControlMode.PercentOutput, 0);
